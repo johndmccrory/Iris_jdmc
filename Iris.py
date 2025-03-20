ef585105f324ddea95791b5eee4da84cad6186a8
johndmccrory <100780899+johndmccrory@users.noreply.github.com>
2025-03-20 14:18:47 +0000

    Add a feature to query and write setosa data to a csv file.

--- Iris.py
+++ Iris.py
@@ -4,8 +4,16 @@ import csv
 if __name__ == "__main__":
     data_path = "Data/Iris.csv"
 
+    setosa_data = []
+
     with open(data_path, 'r', newline='') as input_data:
         reader = csv.DictReader(input_data)
 
         for row in reader:
-            print(row)
+            if "setosa" == row['species']:
+                setosa_data.append(row)
+
+    with open("setosa_data.csv", 'w', newline='') as output:
+        writer = csv.writer(output)
+
+        writer.writerows(setosa_data)
