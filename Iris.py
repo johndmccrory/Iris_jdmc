1bbd8865f788cb7a2384e6a13bd52ddb09e5c33e
johndmccrory <100780899+johndmccrory@users.noreply.github.com>
2025-03-20 15:10:42 +0000

    Fixed a bug writing setosa data to a csv file

--- Iris.py
+++ Iris.py
@@ -9,11 +9,13 @@ if __name__ == "__main__":
     with open(data_path, 'r', newline='') as input_data:
         reader = csv.DictReader(input_data)
 
+        field_names = reader.fieldnames
+
         for row in reader:
             if "setosa" == row['species']:
                 setosa_data.append(row)
 
     with open("setosa_data.csv", 'w', newline='') as output:
-        writer = csv.writer(output)
+        writer = csv.DictWriter(output, fieldnames=field_names)
 
         writer.writerows(setosa_data)
